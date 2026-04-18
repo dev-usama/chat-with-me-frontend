14dcc90ff126a534ace04ad952048210a6cfce0a
Usama Ahmed <minhasusama66@gmail.com>
2026-04-19 02:35:54 +0500

    Improved UI and add a loader for chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css';
 import Message from './Message';
 import { useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
   let [messages, setMessages] = useState([]);
+  let [loading, setLoading] = useState(false);
 
   async function fetchResponse(e) {
     e.preventDefault();
+    setLoading(true);
     try {
       const form = e.target;
       const formData = new FormData(form);
       const formJson = Object.fromEntries(formData.entries());
       const prompt = formJson.prompt
-      console.log("Prompt:", prompt)
       let response = await fetch('http://localhost:8000/ask', {
         method: "POST",
         headers: {
@@ -22,6 +24,7 @@ function App() {
       });
       const data = await response.json();
       setMessages([...messages, { prompt: prompt, response: data }]);
+      setLoading(false);
     } catch (ex) {
       setMessages([...messages, { prompt: 'Fetching info...', response: ex.message }]);
       console.log(ex);
@@ -29,7 +32,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Ask Me Anything</h1>
+      <h1>Hi, I’m Usama 👋</h1>
       <div className='messageContainer'>
         {
           messages.map((message) => {
@@ -41,9 +44,12 @@ function App() {
             );
           })
         }
+        <Button style={{display: loading ? "block" : "None"}} loading variant="outlined">
+          Submit
+        </Button>
       </div>
       <form method='POST' className="prompt-container" onSubmit={fetchResponse}>
-        <input name='prompt' />
+        <input name='prompt' placeholder='Ask me Anything!' required='true' />
         <button className='promptBtn' type='submit'>Send</button>
       </form>
     </div>
